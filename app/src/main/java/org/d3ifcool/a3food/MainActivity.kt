999e84fd0f62fd1165da02a1b8a2a012da658e44
DimasArioTriKusumo <kusumatridim@gmail.com>
2022-10-20 15:57:51 +0700

    Initial Commit

---
+++ app/src/main/java/org/d3ifcool/a3food/MainActivity.kt
@@ -0,0 +1,11 @@
+package org.d3ifcool.a3food
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
