942273b9176f8817dbae6f789c1f1c8a52f845f1
Dwiko Indrawansyah <wikosac@gmail.com>
2022-10-27 15:51:29 +0700

    login error

--- app/src/main/java/org/d3ifcool/a3food/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/a3food/MainActivity.kt
@@ -2,10 +2,41 @@ package org.d3ifcool.a3food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
+import com.google.firebase.auth.FirebaseAuth
+import org.d3ifcool.a3food.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val contract = FirebaseAuthUIActivityResultContract()
+    private val signInLauncher = registerForActivityResult(contract) { this.onSignInResult(it) }
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        binding.googleImageButton.setOnClickListener { mulaiLogin() }
+    }
+
+    private fun mulaiLogin() {
+        val providers = arrayListOf(AuthUI.IdpConfig.GoogleBuilder().build())
+        val intent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(providers)
+            .build()
+        signInLauncher.launch(intent)
+    }
+
+    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
+        val response = result.idpResponse
+        if (result.resultCode == RESULT_OK) {
+            val nama = FirebaseAuth.getInstance().currentUser?.displayName
+            Log.i("LOGIN", "$nama berhasil login")
+        } else {
+            Log.i("LOGIN", "Login gagal: ${response?.error?.errorCode}")
+        }
     }
 }
