f739113d85396a53b3c4a32f99e550fd45868f2f
Dwiko Indrawansyah <wikosac@gmail.com>
2022-11-02 21:44:13 +0700

    Login

--- app/src/main/java/org/d3ifcool/a3food/MainActivity.kt
+++ app/src/main/java/org/d3ifcool/a3food/MainActivity.kt
@@ -16,7 +16,8 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         binding.googleImageButton.setOnClickListener { mulaiLogin() }
     }
