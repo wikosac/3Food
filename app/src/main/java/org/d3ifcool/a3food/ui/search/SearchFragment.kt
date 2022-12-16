83bac3ec6b29b1b62aa1cee871e0fb221dbe814b
Indrawansyah Wiko <wikosac@gmail.com>
2022-12-16 23:01:19 +0700

    keyword validation

--- app/src/main/java/org/d3ifcool/a3food/ui/search/SearchFragment.kt
+++ app/src/main/java/org/d3ifcool/a3food/ui/search/SearchFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.isDigitsOnly
+import androidx.lifecycle.ViewModelProvider
+import org.d3ifcool.a3food.data.FoodDb
 import org.d3ifcool.a3food.databinding.FragmentSearchBinding
+import org.d3ifcool.a3food.ui.dashboard.DashboardViewModel
+import org.d3ifcool.a3food.ui.dashboard.DashboardViewModelFactory
 
 class SearchFragment : Fragment() {
 
     private lateinit var binding: FragmentSearchBinding
+//    private val viewModel: SearchViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +30,27 @@ class SearchFragment : Fragment() {
 
         binding.magniGlass.setOnClickListener {
             val keyword = binding.searchView.text.toString()
-            binding.testOutput.text = keyword
+            val validKey = validation(keyword)
+            binding.testOutput.text = validKey
         }
+    }
+
+    fun validation(key: String): String {
+        // Pengecekan kalo inputnya kosong
+        if (key.isEmpty()) {
+            return "Inputan gaboleh kosong"
+        }
+
+        // kalo inputan berupa angka (tapi lu pake string / text)
+        if (key.isDigitsOnly()) {
+            return "Inputan gabisa angka"
+        }
+
+        // kalo inputan simbol
+        if (key.contains("[!\"#$%&'()*+,-./:;\\\\<=>?@\\[\\]^_`{|}~]".toRegex())) {
+            return "Inputan gabisa simbol"
+        }
+
+        return key
     }
 }
--- app/src/main/java/org/d3ifcool/a3food/ui/search/SearchViewModel.kt
+++ app/src/main/java/org/d3ifcool/a3food/ui/search/SearchViewModel.kt
@@ -1,7 +1,59 @@
 package org.d3ifcool.a3food.ui.search
 
+import androidx.core.text.isDigitsOnly
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.d3ifcool.a3food.data.Food
+import org.d3ifcool.a3food.data.Key
+import org.d3ifcool.a3food.data.KeyDao
+import org.d3ifcool.a3food.databinding.FragmentSearchBinding
 
-class SearchViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SearchViewModel(private val db : KeyDao) : ViewModel() {
+
+    val data = db.getData()
+    private lateinit var binding: FragmentSearchBinding
+
+    fun insertData(key: Key) {
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                db.insertData(key)
+            }
+        }
+    }
+
+    fun deleteData(ids: List<String>) {
+        val newIds = ids.toList()
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                db.deleteData(newIds)
+            }
+        }
+    }
+
+    fun validation(key: String) {
+        // Pengecekan kalo inputnya kosong
+        if (binding.searchView.text?.isEmpty() == true) {
+            showMessage("Inputan gaboleh kosong")
+            return
+        }
+
+        // kalo inputan berupa angka (tapi lu pake string / text)
+        if (binding.searchView.text?.isDigitsOnly() == true) {
+            showMessage("Inputan gabisa angka")
+            return
+        }
+
+        // kalo inputan simbol
+        if (binding.searchView.text?.contains("[!\"#$%&'()*+,-./:;\\\\<=>?@\\[\\]^_`{|}~]".toRegex()) == true) {
+            showMessage("Inputan gabisa simbol")
+            return
+        }
+    }
+
+    fun showMessage(msg: String) {
+        binding.testOutput.text = msg
+    }
 }
